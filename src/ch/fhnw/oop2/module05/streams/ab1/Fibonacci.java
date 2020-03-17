b39daafc8abeee596c06576c17211c0f002d7efc
Luca Lunati (s) <luca.lunati@students.fhnw.ch>
2020-03-17 12:03:19 +0100

    fibonacci 1st try

--- src/ch/fhnw/oop2/module05/streams/ab1/Fibonacci.java
+++ src/ch/fhnw/oop2/module05/streams/ab1/Fibonacci.java
@@ -1,10 +1,12 @@
 package ch.fhnw.oop2.module05.streams.ab1;
 
+import java.util.stream.Stream;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Stream.iterate(new Tuple<Integer>(0, 1), n->new Tuple<Integer>(n.t1+n.t2,n.t2+n.t2+n.t1)).limit(30).forEach(n -> System.out.println(n));
 	}
 
 }
