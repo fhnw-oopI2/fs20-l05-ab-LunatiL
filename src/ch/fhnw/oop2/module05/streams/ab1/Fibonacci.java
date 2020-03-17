264e9126dab4a3b178d279954f1f286e11de1fee
Luca Lunati (s) <luca.lunati@students.fhnw.ch>
2020-03-17 12:06:14 +0100

    fibonacci better output

--- src/ch/fhnw/oop2/module05/streams/ab1/Fibonacci.java
+++ src/ch/fhnw/oop2/module05/streams/ab1/Fibonacci.java
@@ -6,7 +6,10 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Stream.iterate(new Tuple<Integer>(0, 1), n->new Tuple<Integer>(n.t1+n.t2,n.t2+n.t2+n.t1)).limit(30).forEach(n -> System.out.println(n));
+//		Stream.iterate(new Tuple<Integer>(0, 1), n->new Tuple<Integer>(n.t1+n.t2,n.t2+n.t2+n.t1)).limit(30).forEach(n -> System.out.println(n));
+		Stream.iterate(new Tuple<Integer>(0, 1), n -> new Tuple<Integer>(n.t1 + n.t2, n.t2 + n.t2 + n.t1)).limit(30)
+				.forEach(n -> {System.out.println(n.t1);
+				System.out.println(n.t2);});
 	}
 
 }
